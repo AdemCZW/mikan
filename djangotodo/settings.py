d3764739336fb792edf981fdab42fd644c775ba2
adam chen <tmac19920626@gmail.com>
2021-07-28 18:19:14 +0800

    your_message

--- djangotodo/settings.py
+++ djangotodo/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'djangotodo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,5 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static'),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
