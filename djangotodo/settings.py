59af5cf71dc00f56447cefced7058cf5cb8f386a
adam chen <tmac19920626@gmail.com>
2021-08-09 16:13:51 +0800

    0809

--- djangotodo/settings.py
+++ djangotodo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6jm(self_rx4e(^c!132#%pt6f5@s&qv^3l9r+_c#q4qg!w#02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 
@@ -90,7 +90,7 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Todo,Customer,Flight,Movie
+from .models import Todo,Customer,Flight
 
 
 
@@ -19,7 +19,3 @@ class FlightAdmin(admin.ModelAdmin):
 		
 admin.site.register(Flight)
 
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'genre', 'release_year')
- 
-admin.site.register(Movie, MovieAdmin)
